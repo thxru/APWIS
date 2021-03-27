3c522f35f6b3623b734043635cec639c062d779c
Prabhashi Hettiarachchi <63660305+Pubzzz@users.noreply.github.com>
2021-03-27 13:19:56 +0530

    dbCon

---
+++ APWISweb/FirebaseSDK.php
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.3.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.3.1/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAdorDtrdpjMeAXtJGWOTbMhesTdJH6UG4",
+    authDomain: "apwis-a2553.firebaseapp.com",
+    projectId: "apwis-a2553",
+    storageBucket: "apwis-a2553.appspot.com",
+    messagingSenderId: "1089355738874",
+    appId: "1:1089355738874:web:6f67ac72ff9baa423f2343",
+    measurementId: "G-9685906EMP"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
