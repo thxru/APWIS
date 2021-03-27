99b966273d2b243c37a8cebc86af905798a8c7fa
Prabhashi Hettiarachchi <63660305+Pubzzz@users.noreply.github.com>
2021-03-27 13:33:04 +0530

    Update login.php

--- login.php
+++ login.php
@@ -4,15 +4,15 @@
     $password = "";
     $dbname = "apwis";
     $conn = mysqli_connect($servername, $username, $password, $dbname);
-    if (isset($_REQUEST['submit'])) {
-        $nic = $_REQUEST['nic'];
+    if (isset($_REQUEST['//submit//'])) {
+        $nic = $_REQUEST['//nic//'];
         $pass = $_REQUEST['pass'];
-        $query = "select * from registration where nic='$nic' and pass='$pass'";
+        $query = "select * from users where //client='$nic' and password='$pass'//";
         $rs = mysqli_query($conn, $query);
         $rowcount = mysqli_num_rows($rs);
         if ($rowcount == 1) {
             echo '<script type="text/javascript">alert("WELCOME");</script>';
-            header('location:index.html?');
+            header('//location:index.html?//');
         } else {
             echo '<script type="text/javascript">alert("Please check your credentials");</script>';
         }
