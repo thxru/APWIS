84e8276e5c3235037817ba69e9dd9a00e08d0b58
Prabhashi Hettiarachchi <63660305+Pubzzz@users.noreply.github.com>
2021-03-27 13:31:56 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "apwis";
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    if (isset($_REQUEST['submit'])) {
+        $nic = $_REQUEST['nic'];
+        $pass = $_REQUEST['pass'];
+        $query = "select * from registration where nic='$nic' and pass='$pass'";
+        $rs = mysqli_query($conn, $query);
+        $rowcount = mysqli_num_rows($rs);
+        if ($rowcount == 1) {
+            echo '<script type="text/javascript">alert("WELCOME");</script>';
+            header('location:index.html?');
+        } else {
+            echo '<script type="text/javascript">alert("Please check your credentials");</script>';
+        }
+    }
+    mysqli_close($conn);
+    ?>
